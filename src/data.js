212362502c9fcc76b2bbeeb55794b04125625980
FJAsayag <Asayag2018@hotmail.com>
2019-03-26 09:02:58 -0500

    new data array created

--- src/data.js
+++ src/data.js
@@ -1,19 +1,31 @@
 const newInjuries = [];
-const indexInjuries = [{name: 'Ocupantes de Bus', index: 2},{name : 'Motociclistas', index : 13}, {name: 'Pasajeros de auto', index: 20},{name: 'Ciclistas', index: 23}, {name: 'Peatones', index: 24}, {name: 'Botes recreacionales', index: 36}, {name: 'Año', index: 48}];
+const indexInjuries = [
+  {name: 'Ocupantes de Bus', index: 2, 
+  imageI: 1},
+  {name : 'Motociclistas', index : 13, 
+  imageI: 2}, 
+  {name: 'Pasajeros de auto', index: 20, 
+  imageI: 3},
+  {name: 'Ciclistas', index: 23, 
+  imageI: 4}, 
+  {name: 'Peatones', index: 24, 
+  imageI: 5}, 
+  {name: 'Botes recreacionales', index: 36, 
+  imageI: 6}, 
+  {name: 'Año', index: 48, 
+  imageI: 7}
+];
 
 for(let i = 0; i < INJURIES.length; i++) {
     const obj = {}
     for(let j = 0; j < indexInjuries.length; j++)  {
-        obj[indexInjuries[j].name] = Object.values(INJURIES[i])[indexInjuries[j].index]
-    } 
+        obj[indexInjuries[j].name] = Object.values(INJURIES[i])[indexInjuries[j].index];
+    // objeto[ P R O P I E D A D ] =  V A L O R  //
+    }
     newInjuries.push(obj)
 }
 console.log(newInjuries)
-
-
-
-
-
+newInjuries
 
 
 
