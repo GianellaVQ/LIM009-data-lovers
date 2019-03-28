f34f0a806fe10aa5ca2e0d2d6c216b34d51d4758
FJAsayag <Asayag2018@hotmail.com>
2019-03-28 15:15:01 -0500

    DOM manipulation implemented

--- src/main.js
+++ src/main.js
@@ -1,22 +1,17 @@
+const injHeader = document.getElementById("injuries-header");
+injHeader.style.display = "none";
+const decisionScreen = document.getElementById("decision-section");
+decisionScreen.style.display = "none";
+
+document.getElementById("go-to-folios").addEventListener("click", () => {
+document.getElementById("first-view").style.display = "none";
+injHeader.style.display = "flex";
+decisionScreen.style.display = "flex";
+});
+
+document.getElementById("go-to-by-years").addEventListener("click", () => {
+decisionScreen.style.display = "none";
+
 const divPainting = document.getElementById("pared");
-let newDiv= [];
-
-for (let i = 0; i < newInjuries.length; i++) {
-
-    newDiv[i] = document.createElement("DIV");
-    newDiv[i].className = "container";
-
-    let yearTemp = `
-        <article>
-            <p> Año: ${newInjuries[i]['Año']} .</p>
-            <p> Botes recreacionales: ${newInjuries[i]['Botes recreacionales']} .</p>
-            <p> Ciclistas: ${newInjuries[i]['Ciclistas']} .</p>
-            <p> Ocupantes de bus: ${newInjuries[i]['Ocupantes de bus']} .</p>
-            <p> Motociclistas: ${newInjuries[i]['Motociclistas']} .</p>
-            <p> Peatones: ${newInjuries[i]['Peatones']} .</p>
-            <p> Pasajeros de auto: ${newInjuries[i]['Pasajeros de auto']} .</p>
-        </article>
-    `
-    newDiv[i].innerHTML = yearTemp;
-    divPainting.appendChild(newDiv[i]);
-}
+mostrar(newInjuries, divPainting)
+});
