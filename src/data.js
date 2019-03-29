cd71784e47457a3d8104027502fd065dda961fd4
FJAsayag <Asayag2018@hotmail.com>
2019-03-29 11:58:00 -0500

    showing data by filter of years or indicators

--- src/data.js
+++ src/data.js
@@ -1,34 +1,23 @@
-const newInjuries = [];
-const indexInjuries = [
-  {name: 'Ocupantes de Bus', index: 2, 
-  imageI: 1},
-  {name : 'Motociclistas', index : 13, 
-  imageI: 2}, 
-  {name: 'Pasajeros de auto', index: 20, 
-  imageI: 3},
-  {name: 'Ciclistas', index: 23, 
-  imageI: 4}, 
-  {name: 'Peatones', index: 24, 
-  imageI: 5}, 
-  {name: 'Botes recreacionales', index: 36, 
-  imageI: 6}, 
-  {name: 'Año', index: 48, 
-  imageI: 7}
-];
+const newData = (data) => {
+  let newInjuries = [];
 
-for(let i = 0; i < INJURIES.length; i++) {
-    const obj = {}
-    for(let j = 0; j < indexInjuries.length; j++)  {
-        obj[indexInjuries[j].name] = Object.values(INJURIES[i])[indexInjuries[j].index];
-    // objeto[ P R O P I E D A D ] =  V A L O R  //
-    }
-    newInjuries.push(obj)
-}
-console.log(newInjuries)
-newInjuries
+  for (let i = 0; i < data.length; i++) {
+    const newObj = {
+      'Año' : data[i].Year,
+      'Tripulantes' : data[i].Total_Injured_Persons_Recreational_Boating === null ? '--' : data[i].Total_Injured_Persons_Recreational_Boating,
+      'Ciclistas' : data[i].Total_Injured_Persons_Pedalcyclists === null ? '--' : data[i].Total_Injured_Persons_Pedalcyclists,
+      'Ocupantes de bus' : data[i].Total_Injured_Persons_Bus_Occupants === null ? '--' : data[i].Total_Injured_Persons_Bus_Occupants,
+      'Motociclistas' : data[i].Total_Injured_Persons_Motorcyclists === null ? '--' : data[i].Total_Injured_Persons_Motorcyclists,
+      'Peatones' : data[i].Total_Injured_Persons_Pedestrians === null ? '--' : data[i].Total_Injured_Persons_Pedestrians,
+      'Pasajeros de auto' : data[i].Total_Injured_Persons_Passenger_Car_Occupants === null ? '--' : data[i].Total_Injured_Persons_Passenger_Car_Occupants,  
+    };
+    newInjuries.push(newObj);
+  }
+  return newInjuries
+};
 
 
-const mostrar = (data, divPainted) => {
+const showByYear = (data, sectionPainted) => {
   let newDiv= [];
   for (let i = 0; i < data.length; i++) {
 
@@ -38,7 +27,7 @@ const mostrar = (data, divPainted) => {
       let yearTemp = `
           <article>
               <p> Año: ${data[i]['Año']} .</p>
-              <p> Botes recreacionales: ${data[i]['Botes recreacionales']} .</p>
+              <p> Tripulantes: ${data[i]['Tripulantes']} .</p>
               <p> Ciclistas: ${data[i]['Ciclistas']} .</p>
               <p> Ocupantes de bus: ${data[i]['Ocupantes de bus']} .</p>
               <p> Motociclistas: ${data[i]['Motociclistas']} .</p>
@@ -47,45 +36,55 @@ const mostrar = (data, divPainted) => {
           </article>
       `
       newDiv[i].innerHTML = yearTemp;
-      divPainted.appendChild(newDiv[i]);
+      sectionPainted.appendChild(newDiv[i]);
   }
 }
 
+const filterByYear = (data, year) => {
+  let result = [];
+  for(let i = 0; i < data.length; i++) {
+    if (parseInt(year) === parseInt(data[i]['Año'])){
+      result.push(data[i]);
+    }
+  }
+  return result;
+}
 
+const filterByIndicator = (data, indicator) => {
+  //creamos un array que tendrá los valores del indicador seleccionado en todos los años
+  let arr = [];
+  for(let j = 0; j < data.length; j++){
+    // j = 0, "{año: 1960", tripulantes: 63254, ...}
+    for(let k = 0; k < Object.keys(data[j]).length; k++){
+     // k = 0, 'año'  >>> k = 1, 'tripulates'
+      if(indicator === Object.keys(data[j])[k]){
+        if(typeof Object.values(data[j])[k] === 'number')
+       arr.push(Object.values(data[j])[k])
+      }
+    }
+  }
+  return arr
+}
 
+const indTotalSum = (arrValues) => {
+  let sum = 0;
+  for(let i = 0; i < arrValues.length; i++) {
+    sum+= parseInt(arrValues[i])
+  }
+  return sum
+}
 
+const indAverage = (totalSumOfInd, arrAddends) => {
+  return Math.round(totalSumOfInd/arrAddends.length);
 
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// window.data = {
-//   filterByYear: (data, year) => {
-//     return data.filter((obj) => parseInt(obj.Year) === year);
-//   }
-
-//   }
-
-
-  //console.log(data.filterByYear(INJURIES,2015))
-
+const showAverageByIndicator = (indicator, average, sectionPainted) => {
+  let newDiv = document.createElement("DIV")
+  let avrTemp = `
+      <h2>${indicator}</h2>
+      <p>En promedio, anualmente, han habido ${average} ${indicator} heridos. </p>
+  `
+  newDiv.innerHTML = avrTemp;
+  sectionPainted.appendChild(newDiv);
+}
